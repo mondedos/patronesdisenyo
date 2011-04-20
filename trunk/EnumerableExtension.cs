a2daf7ae0a7f9c0ec5efee62543d96e41e6c08e5
ricardo.prietomendoza <ricardo.prietomendoza@cf1b75cc-a63a-0410-8382-8b98c2212edd>
2011-04-20 07:15:00 +0000

    Cambio de espacio de nombres.

--- trunk/EnumerableExtension.cs
+++ trunk/EnumerableExtension.cs
@@ -1,9 +1,9 @@
-﻿using System;
+ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace ConsoleApplication1
+namespace System.Collections.Generic
 {
     public static class EnumerableExtension
     {
